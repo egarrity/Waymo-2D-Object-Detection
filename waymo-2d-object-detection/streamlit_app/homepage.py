4924df1a18d6bc6248ef5a3c7b5d3f644ad814b5
123tshi <thshi@stanford.edu>
2021-02-15 00:50:01 -0800

    basic upload + centernet predict bbox viz

--- waymo-2d-object-detection/streamlit_app/homepage.py
+++ waymo-2d-object-detection/streamlit_app/homepage.py
@@ -1,11 +1,13 @@
+
+
 import time
 import streamlit as st
 
 import numpy as np 
 import pandas as pd 
 import cv2
-
-
+"""
+#peter's code (-:
 def app():
     st.title("Welcome to Object Detection Central")
     st.header("Detecting cars, cyclists and pedestrians 🚗🚴‍♂🚶‍♂️")
@@ -20,5 +22,60 @@ def app():
     })
 
     expander = st.beta_expander("FAQ")
-    expander.write("Here you could put in some really, really long explanations...")
+    expander.write("Here you could put in some really, really long explanations...")"""
+    
+    
+    
+import PIL.Image as Image
+from gluoncv import model_zoo, data, utils
+from matplotlib import pyplot as plt
+import streamlit as st
+
+
+def vis_image_with_bbox(imgfile):
+    net = model_zoo.get_model('center_net_resnet18_v1b_voc', pretrained=True)
+    #im_fname = utils.download('https://raw.githubusercontent.com/zhreshold/' +
+    #                          'mxnet-ssd/master/data/demo/dog.jpg',
+    #                          path='dog.jpg')
+    x, img = data.transforms.presets.center_net.load_test("tmpImgFile.jpg", short=512)
+    print('Shape of pre-processed image:', x.shape)
+
+    class_IDs, scores, bounding_boxs = net(x)
+    #fig = plt.figure()
+    ax = utils.viz.plot_bbox(img, bounding_boxs[0], scores[0],
+                             class_IDs[0], class_names=net.classes)
+    fig = plt.gcf()
+    #fig.axes.append(ax)
+    plt.draw()
+    fn = 'tmpfile.png'
+    fig.savefig(fn)
+
+    return fn
+
+
+def app():
+    st.title("Welcome to Object Detection Central")
+    st.header("Detecting cars, cyclists and pedestrians 🚗🚴‍♂🚶‍♂️")
+    topbox = st.sidebar.selectbox("Choose what to do ", ['Inspect Predictions Visually',
+                                                         'Other Options'
+                                                         ])
+
+    if topbox == 'Inspect Predictions Visually':
+        st.sidebar.subheader('Inspect Predictions')
+
+        uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            image.save('tmpImgFile.jpg')
+            st.image(image, caption='Uploaded Image.', use_column_width=True)
+            st.write("")
+            st.write("Classifying...")
+            fn = vis_image_with_bbox(image)
+            st.image(fn, use_column_width=True)
+
+
+if __name__ == '__main__':
+    app()
+
+   
 
